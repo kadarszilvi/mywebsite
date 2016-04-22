51d883b8d53d0aa1a05e6c964d283dcc937fa300
kadarszilvi <kadarszilvi@gmail.com>
2016-04-22 16:07:08 +0200

    first app, gitignore

---
+++ http_server.py
@@ -0,0 +1,7 @@
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
